52242a52772f950463890fe393733b50697afc84
yahiaprince <yelprinse94@gmail.com>
2024-10-23 22:41:37 +0300

    final

--- LibrarySystem.cpp
+++ LibrarySystem.cpp
@@ -259,36 +259,23 @@ public:
                 break;
         }
     }
-
     int menu()
     {
-        int choice = -1;
-        while (choice == -1)
-        {
-            if (false)
-            {
-                cout << "\nLibrary Menu;\n";
-                cout << "1) add_book\n";
-                cout << "2) search_books_by_prefix\n";
-                cout << "3) print_who_borrowed_book_by_name\n";
-                cout << "4) print_library_by_id\n";
-                cout << "5) print_library_by_name\n";
-                cout << "6) add_user\n";
-                cout << "7) user_borrow_book\n";
-                cout << "8) user_return_book\n";
-                cout << "9) print_users\n";
-                cout << "10) Exit\n";
-            }
+        cout << "\nLibrary Menu:\n";
+        cout << "1) Add New Book\n";
+        cout << "2) Search Books By Prefix\n";
+        cout << "3) Print Who Borrowed Book By Name\n";
+        cout << "4) Print Library By ID\n";
+        cout << "5) Print Library By Name\n";
+        cout << "6) Add User\n";
+        cout << "7) User Borrow Book\n";
+        cout << "8) User Return Book\n";
+        cout << "9) Print Users\n";
+        cout << "10) Exit\n";
 
-            cout << "\nEnter your menu choice [1 - 10]: ";
-            cin >> choice;
-
-            if (!(1 <= choice && choice <= 10))
-            {
-                cout << "Invalid choice. Try again\n";
-                choice = -1; // loop keep working
-            }
-        }
+        cout << "\nEnter your menu choice [1 - 10]: ";
+        int choice;
+        cin >> choice;
         return choice;
     }
 
